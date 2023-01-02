0e570b5c885410f32d7420645a99f8ef7f1515bb
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-02 16:54:55 -0300

    Create atribuicoes.js
    Estudo de atribuições no javascript

---
+++ atribuicoes.js
@@ -0,0 +1,18 @@
+const a = 7 
+let b = 3
+
+//Todos os exemplos abaixo são tipos de atribuição.
+b += a //b = b + a
+console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
