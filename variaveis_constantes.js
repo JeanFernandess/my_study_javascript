c37b1d95328722df01ac77eaf45b04f9e5deca62
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-02 16:45:05 -0300

    Create variaveis_constantes.js
    Estudando constantes, var e let

---
+++ variaveis_constantes.js
@@ -0,0 +1,13 @@
+var a = 10;   //O uso do VAR serve para declarar variaveis, mas não e recomendado
+let b = 20;   //O uso do LET e o recomendado para declarar variaveis, porem com o LET, não podemos redeclarar variaveis.
+
+console.log(a, b);
+
+a = 200;
+b = 300;
+
+console.log(a, b);
+
+const c = 200;//A palavra CONST serve para declarar constantes.
+
+console.log(c);
