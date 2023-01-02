f2d76891712554e5a9c6850f57b6b76e9081a591
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-01 22:22:50 -0300

    Create test_var2.js
    Estudando Var(Escopo de Variaveis)

---
+++ test_var2.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+
+    var numero = 2 //Essa variavel vai subescrever a variavel inicial, mostrando na tela 'Dentro' e 'fora' com resultado 2.
+    console.log('Dentro = ', numero)
+}
+
+console.log('Fora = ', numero)
