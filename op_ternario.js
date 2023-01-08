37cf745e046cb2e8748df2fa2f78efca0757708e
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-08 19:17:34 -0300

    Create op_ternario.js
    Estudo do operador ternário no javascript

---
+++ op_ternario.js
@@ -0,0 +1,11 @@
+const resultado = nota => nota >= 7 ?'Aprovado':'Reprovado' //EXEMPLO DE OPERADOR TERNÁRIO
+
+console.log(resultado(7.3))
+console.log(resultado(5.0))
+
+//const status = nota >= 7 ? 'Aprovado' : 'Reprovado' //PODE SER FEITO DESSA FORMA
+
+/*PODE SER FEITO DESSA FORMA.
+const resultado = nota => {
+    return nota >= 7 ? 'Aprovado':'Reprovado'
+} */
