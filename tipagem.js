40ac1383f7eb4b5971c660cfa55d9eb81ea67520
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-15 22:08:05 -0300

    Create tipagem.js
    Estudo de tipagem no javascript

---
+++ tipagem.js
@@ -0,0 +1,12 @@
+let nome = "Jean"; //Essa variavel recebe um string
+console.log(nome);
+console.log(typeof nome);// A função TYPEOF serve para mostrar o tipo da variavel
+
+// A tipagem do javascript e fraca, não necessitando especificar o tipo de variavel(Se é string, float, boolean ou inteiro).
+
+let nome2 = "Ola, mundo"; //Nome guarda uma string.Sem o 2, ocasionaria em um erro por tentar sobrescrever a variavel.
+let numero = 10; //Numero guarda um number ou inteiro.
+let altura = 1.82; //Altura guarda um float(Numero com ponto flutuante).
+let mentira = false;// Mentira guarda um boolean(True ou False).
+
+//As variaveis acima mostram algumas tipagem do javascript;
