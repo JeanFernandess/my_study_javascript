0f2a78b7973355a34b3dd9723e1bed75f8d58dab
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-15 22:06:47 -0300

    Create tipo_number_cuidados.js
    Cuidados com o uso de number no javascript

---
+++ tipo_number_cuidados.js
@@ -0,0 +1,4 @@
+console.log(7 / 0); // Cuidado com divisões por zero.
+console.log("10" / 2) //Cuidado com operações com string.
+console.log("Jean" / 2) //Causará um erro(NaN)não é um numero.
+console.log(0.1 + 0.7) //Operaçõe fracionadas podem gerar uma imprecisão.
