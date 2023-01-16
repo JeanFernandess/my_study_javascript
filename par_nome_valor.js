2e7a7bcbbf5ef38a7d3aecb5ae6db375b508adc4
Jean Fernandes Batista <47990175+JeanFernandess@users.noreply.github.com>
2023-01-15 22:10:16 -0300

    Create par_nome_valor.js
    Estudo de pares, nome e valores com javascrip

---
+++ par_nome_valor.js
@@ -0,0 +1,25 @@
+//Par Nome/Valor
+
+const saudacao = 'Opa'// Contexto léxico 1
+ 
+function exec() {
+    const saudacao = 'Falaa' //contexto léxico 2
+    return saudacao
+}
+
+
+//Objetos são grupos aninhados de pares nome/valor
+
+const cliente = {
+    nome: 'Pedro',
+    idade: 23,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
